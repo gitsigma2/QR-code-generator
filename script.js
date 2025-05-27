2b4e01307b0b98a2dd4a89a792f95a58755fb97d
Bikram <iqsigma2@gmail.com>
2025-05-27 23:21:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = document.querySelector('#input')
+let btnGenerate = document.querySelector('#generateQR')
+let imgQR = document.querySelector('#qrCodeImage')
+
+function generateQR() {
+     if(input.value === '') {
+        alert('Please enter a value to generate a QR code.')
+        return
+    }
+    imgQR.setAttribute('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + input.value)
+}
+
+btnGenerate.addEventListener('click', generateQR)
+input.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        generateQR();
+    }
+});
